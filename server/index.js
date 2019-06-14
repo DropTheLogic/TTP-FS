24d2430af1a33ab98643e0ead86fae93fceca80d
Daniel Marquez <DannyBrianMarquez@gmail.com>
2019-06-14 15:09:04 -0400

    feat: Add simple server

---
+++ server/index.js
@@ -0,0 +1,28 @@
+const express = require('express');
+const morgan = require('morgan');
+const path = require('path');
+
+const PORT = process.env.PORT || 8080;
+
+const app = express();
+
+// Logging and middleware
+app.use(morgan('dev'));
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+app.use(express.static(path.join(__dirname, '../public')));
+
+// Send index.html
+app.use('*', (req, res, next) => {
+	res.sendFile(path.join(__dirname, '../public/index.html'));
+});
+
+// Error Handling
+app.use((error, req, res, next) => {
+	console.error(error);
+	console.error(error.stack);
+	res.status(500).send(error.message || '500: Internal Error');
+});
+
+// Start server listeng
+app.listen(PORT, () => console.log(`Listening on port ${PORT}`));
