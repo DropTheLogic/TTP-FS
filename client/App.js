2aee8a8efa18b5fccec6f9e23756b3219da67180
Daniel Marquez <DannyBrianMarquez@gmail.com>
2019-06-18 11:48:34 -0400

    feat: Add simple routing for auth sign in and register

--- client/App.js
+++ client/App.js
@@ -1,21 +1,40 @@
-import React, { Component } from 'react';
+import React, { Component, Fragment } from 'react';
+import { Switch, Route } from 'react-router-dom';
 import { Container } from 'semantic-ui-react';
 
 import {
 	Header,
 	Nav,
-	Login
+	Login,
+	Portfolio,
+	Transactions
 } from './components';
 
 
 class App extends Component {
 	render() {
 		return (
-			<Container>
+			<Fragment>
 				<Header />
-				<Nav />
-				<Login />
-			</Container>
+				<Container>
+				{
+					this.props.isLoggedIn && (
+					<Fragment>
+						<Nav />
+						<Switch>
+							<Route path="/portfolio" component={Portfolio} />
+							<Route path="/transactions" component={Transactions} />
+						</Switch>
+					</Fragment>
+				)}
+					<Switch>
+						<Route path="/register" render={props => <Login {...props} authType="register" />} />
+						<Route path="/login" render={props => <Login {...props} authType="login" />} />
+						{/** Default to registration */}
+						<Route render={props => <Login {...props} authType="register" />} />
+					</Switch>
+				</Container>
+			</Fragment>
 		);
 	}
 }
--- client/components/Login.js
+++ client/components/Login.js
@@ -1,28 +1,48 @@
-import React from 'react';
-import { Form, Button, Message } from 'semantic-ui-react';
+import React, { Fragment } from 'react';
+import { Form, Button, Message, Icon, Item } from 'semantic-ui-react';
 
-const Login = () => {
+const Login = ({ authType, authError }) => {
 	return (
-		<section>
-			<Message
-				icon="money"
-				header="Register Now for $5000!"
-				content="Sign up now for a one-time gift to grow your porfolio." />
-			<Form>
-				<Form.Field>
-					<label>Full Name:</label>
-					<input placeholder="Full Name" />
-				</Form.Field>
+		<section id="auth">
+			<h2>{authType === 'register' ? 'Register' : 'Sign In' }</h2>
+			{
+				authType === 'register' &&
+				<Message
+					attached
+					icon="money"
+					header="Register Now for $5000!"
+					content="Sign up now for a one-time gift to grow your porfolio." />
+			}
+			<Form className="attached fluid segment">
+				{ authType === 'register' &&
+				<Fragment>
+					<Item>Already have an account? Log in here.</Item>
+					<Form.Field>
+						<label>Full Name:</label>
+						<input placeholder="Full Name" />
+					</Form.Field>
+				</Fragment>
+
+				}
 				<Form.Field>
 					<label>Email:</label>
 					<input placeholder="Email" />
 				</Form.Field>
 				<Form.Field>
 					<label>Password:</label>
-					<input placeholder="Password" />
+					<input placeholder="Password" type="password" />
 				</Form.Field>
-				<Button type="submit" color="green">Submit</Button>
+				<Button type="submit" color="green">
+					{ authType === 'register' ? 'Register' : 'Log In' }
+				</Button>
 			</Form>
+			{
+				authError &&
+				<Message attached="bottom" error>
+					<Icon name="x" />
+					Invalid email or password, please try again.
+				</Message>
+			}
 		</section>
 	);
 };
