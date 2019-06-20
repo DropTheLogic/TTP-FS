4580d64f2406f651bf8a53f4be4ccc39489af9b4
Daniel Marquez <DannyBrianMarquez@gmail.com>
2019-06-19 21:23:00 -0400

    feat: Add reducer for stock price setting

--- client/store/index.js
+++ client/store/index.js
@@ -3,8 +3,9 @@ import thunkMiddleware from 'redux-thunk';
 import { createLogger } from 'redux-logger';
 
 import user from './user';
+import stocks from './stocks';
 
-const reducer = combineReducers({user});
+const reducer = combineReducers({user, stocks});
 const middleware = applyMiddleware(thunkMiddleware, createLogger({collapsed: true}));
 const store = createStore(reducer, middleware);
 
---
+++ client/store/stocks.js
@@ -0,0 +1,64 @@
+import axios from 'axios';
+import { IEX_PUB_KEY } from '../../secrets';
+const iexBaseUrl = `https://cloud.iexapis.com/stable`;
+const token = `token=${IEX_PUB_KEY}`;
+const filter = 'filter=symbol,open,latestPrice,';
+
+let defaultStocks = {};
+
+// Actions
+const GET_ALL_STOCKS = 'GET_ALL_STOCKS';
+
+// Action Creators
+const getAllStocks = stocks => ({ type: GET_ALL_STOCKS, stocks });
+
+// Thunks
+export const getAllStocksThunk = () => async dispatch => {
+	try {
+		// Get my current portfolio of stocks
+		let { data: portfolio } = await axios.get('/api/portfolio');
+		let symbols = Object.keys(portfolio);
+
+		// Get prices for stocks
+		const baseUrl = `${iexBaseUrl}/stock/market/batch`;
+		const symbolArg = `symbols=${symbols.join(',')}`;
+		const types = 'types=quote';
+		const url = `${baseUrl}?${symbolArg}&${types}&${filter}&${token}`;
+		let { data: stocks } = await axios.get(url);
+
+		// Transform API response shape
+		let stockSymbols = Object.keys(stocks);
+		let transformedStocks = stockSymbols.reduce((obj, symbol) => {
+			obj[symbol] = { ...stocks[symbol].quote, quantity: portfolio[symbol].quantity } ;
+			return obj;
+		}, {});
+
+		dispatch(getAllStocks(transformedStocks));
+	} catch (error) {
+		console.error(error);
+	}
+};
+
+// Reducer
+const stocks = (state = defaultStocks, action) => {
+	switch (action.type) {
+		case GET_ALL_STOCKS:
+			return action.stocks;
+		default:
+			return state;
+	}
+};
+
+export default stocks;
+
+
+// Stand-alone price check function, calls API for price info
+export const getOnePrice = async symbol => {
+	try {
+		let url = `${iexBaseUrl}/stock/${symbol}/quote?${token}&${filter}`;
+		let { data: stock } = await axios.get(url);
+		return stock;
+	} catch (error) {
+		console.error(error);
+	}
+};
